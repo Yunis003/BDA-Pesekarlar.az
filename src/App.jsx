f23b1af1a430ca048f8b7cbf3189a14e5eb5f444
Yunis003 <hi.yunisqulamov@gmail.com>
2025-06-05 22:09:34 +0400

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './Components/Header';
+import HeroSection from './Components/HeroSection';
 import './App.css';
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './hero.css';
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <div className="hero-overlay">
+        <div className="hero-content">
+          <h1>İşinin peşəkarını axtarırsınız?</h1>
+          <p>Yüzlərlə peşəkar arasından sizə uyğun olanı tapın.</p>
+          <button className="hero-button">Peşəkarlarla tanış ol</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
