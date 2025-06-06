54da826ebc6b7d8e705657596e258058a45db493
Gulamov Yunis <139101778+Yunis003@users.noreply.github.com>
2025-06-06 17:29:45 +0400

    Flags section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Components/Header';
 import HeroSection from './Components/HeroSection';
 import Categories from './Components/Categories';
+import Flags from './Components/Flags';
 import './App.css';
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Header />
       {/* <HeroSection /> */}
       <Categories />
+      <Flags />
+      {/* <Footer /> */}
     </div>
   );
 }
