394c98497220937648932270a92a0f44336525a4
Yunis003 <hi.yunisqulamov@gmail.com>
2025-06-08 22:39:48 +0400

    Stats Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Flags from './Components/Flags';
 import Card from './Components/Card';
 import JoinUsCard from './Components/JoinUsCard';
 import Comments from './Components/Comments';
+import StatsBanner from './Components/StatsBanner';
 import './App.css';
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Categories />
       <Flags />
       <Card />
+      <StatsBanner />
       <Comments />
       <JoinUsCard />
       {/* <Footer /> */}
